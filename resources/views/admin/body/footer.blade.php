2aaa078d4630dc797a2b80a9be01fd4f90f40ea6
Đặng Ngọc Duy <49876587+pmn2wodee@users.noreply.github.com>
2021-08-17 20:51:55 +0700

    Update

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,12 +2,12 @@
     <div class="pull-right d-none d-sm-inline-block">
         <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
 		  <li class="nav-item">
-			<a class="nav-link" href="javascript:void(0)">FAQ</a>
+			<a class="nav-link" href="https://facebook.com/duydnsieudeptrai">FB</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link" href="#">Purchase Now</a>
+			<a class="nav-link" href="https://duydn.ml">Author</a>
 		  </li>
 		</ul>
     </div>
-	  &copy; 2020 <a href="#">Psd to Html Expert</a>. All Rights Reserved.
+	  &copy; 2021 <a href="#">E-Commerce Website</a>. All Rights Reserved.
   </footer>
