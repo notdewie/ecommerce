4c5b5923eaa1df496232919ed20a7503abc78221
Đặng Ngọc Duy <49876587+pmn2wodee@users.noreply.github.com>
2021-08-28 16:01:57 +0700

    update manage product

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -8,7 +8,9 @@ use App\Models\Brand;
 use App\Models\Product;
 use App\Models\Category;
 use App\Models\MultiImg;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
+use App\Models\SubSubCategory;
 use App\Http\Controllers\Controller;
 
 class ProductController extends Controller
@@ -87,6 +89,138 @@ class ProductController extends Controller
 		);
 
 		return redirect()->back()->with($notification);
-        
     }
+    
+    public function ManageProduct(){
+        $products = Product::latest()->get();
+        return view('backend.product.product_view',compact('products'));
+    }
+
+    public function EditProduct($id){
+        $multiImgs = MultiImg::where('product_id',$id)->get();
+		$categories = Category::latest()->get();
+		$brands = Brand::latest()->get();
+		$subcategory = SubCategory::latest()->get();
+		$subsubcategory = SubSubCategory::latest()->get();
+		$products = Product::findOrFail($id);
+		return view('backend.product.product_edit',compact('categories','brands','subcategory','subsubcategory','products','multiImgs'));
+
+	}
+
+    public function ProductDataUpdate(Request $request){
+
+		$product_id = $request->id;
+
+         Product::findOrFail($product_id)->update([
+      	'brand_id' => $request->brand_id,
+      	'category_id' => $request->category_id,
+      	'subcategory_id' => $request->subcategory_id,
+      	'subsubcategory_id' => $request->subsubcategory_id,
+      	'product_name_en' => $request->product_name_en,
+      	'product_name_vi' => $request->product_name_vi,
+      	'product_slug_en' =>  strtolower(str_replace(' ', '-', $request->product_name_en)),
+      	'product_slug_vi' => str_replace(' ', '-', $request->product_name_vi),
+      	'product_code' => $request->product_code,
+
+      	'product_qty' => $request->product_qty,
+      	'product_tags_en' => $request->product_tags_en,
+      	'product_tags_vi' => $request->product_tags_vi,
+      	'product_size_en' => $request->product_size_en,
+      	'product_size_vi' => $request->product_size_vi,
+      	'product_color_en' => $request->product_color_en,
+      	'product_color_vi' => $request->product_color_vi,
+
+      	'selling_price' => $request->selling_price,
+      	'discount_price' => $request->discount_price,
+      	'short_descp_en' => $request->short_descp_en,
+      	'short_descp_vi' => $request->short_descp_vi,
+      	'long_descp_en' => $request->long_descp_en,
+      	'long_descp_vi' => $request->long_descp_vi,
+
+      	'hot_deals' => $request->hot_deals,
+      	'featured' => $request->featured,
+      	'special_offer' => $request->special_offer,
+      	'special_deals' => $request->special_deals,      	 
+      	'status' => 1,
+      	'created_at' => Carbon::now(),   
+
+      ]);
+
+          $notification = array(
+			'message' => 'Product Updated Without Image Successfully',
+			'alert-type' => 'success'
+		);
+
+		return redirect()->route('manage-product')->with($notification);
+
+
+	} // end method 
+
+    /// Multiple Image Update
+	public function MultiImageUpdate(Request $request){
+		$imgs = $request->multi_img;
+
+		foreach ($imgs as $id => $img) {
+	    $imgDel = MultiImg::findOrFail($id);
+	    unlink($imgDel->photo_name);
+
+    	$make_name = hexdec(uniqid()).'.'.$img->getClientOriginalExtension();
+    	Image::make($img)->resize(917,1000)->save('upload/products/multi-image/'.$make_name);
+    	$uploadPath = 'upload/products/multi-image/'.$make_name;
+
+    	MultiImg::where('id',$id)->update([
+    		'photo_name' => $uploadPath,
+    		'updated_at' => Carbon::now(),
+
+    	]);
+
+	 } // end foreach
+
+       $notification = array(
+			'message' => 'Product Image Updated Successfully',
+			'alert-type' => 'info'
+		);
+
+		return redirect()->back()->with($notification);
+
+	} // end mehtod 
+
+    public function ThambnailImageUpdate(Request $request){
+        $pro_id = $request->id;
+        $oldImage = $request->old_img;
+        unlink($oldImage);
+   
+       $image = $request->file('product_thumbnail');
+           $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+           Image::make($image)->resize(917,1000)->save('upload/products/thumbnail/'.$name_gen);
+           $save_url = 'upload/products/thumbnail/'.$name_gen;
+   
+           Product::findOrFail($pro_id)->update([
+               'product_thumbnail' => $save_url,
+               'updated_at' => Carbon::now(),
+   
+           ]);
+   
+            $notification = array(
+               'message' => 'Product Image Thambnail Updated Successfully',
+               'alert-type' => 'info'
+           );
+   
+           return redirect()->back()->with($notification);
+   
+        } // end method
+
+        public function MultiImageDelete($id){
+            $oldimg = MultiImg::findOrFail($id);
+            unlink($oldimg->photo_name);
+            MultiImg::findOrFail($id)->delete();
+   
+            $notification = array(
+               'message' => 'Product Image Deleted Successfully',
+               'alert-type' => 'success'
+           );
+   
+           return redirect()->back()->with($notification);
+   
+        }
 }
--- routes/web.php
+++ routes/web.php
@@ -128,5 +128,17 @@ Route::prefix('product')->group(function(){
     Route::get('/add',[ProductController::class,'AddProduct'])->name('add-product');
     
     Route::post('/store',[ProductController::class,'StoreProduct'])->name('product-store');
+
+    Route::get('/manage',[ProductController::class,'ManageProduct'])->name('manage-product');
+
+    Route::get('/edit/{id}', [ProductController::class, 'EditProduct'])->name('product.edit');
+
+    Route::post('/data/update', [ProductController::class, 'ProductDataUpdate'])->name('product-update');
+
+    Route::post('/image/update', [ProductController::class, 'MultiImageUpdate'])->name('update-product-image');
+
+    Route::post('/thumbnail/update', [ProductController::class, 'ThumbnailImageUpdate'])->name('update-product-thumbnail');
+
+    Route::get('/multiimg/delete/{id}', [ProductController::class, 'MultiImageDelete'])->name('product.multiimg.delete');
 });
 
